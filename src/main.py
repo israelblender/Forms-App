29688cb955e1a6437da237eb0493e44b9df9e5fe
israel <modelador.criar3d@gmail.com>
2018-08-26 15:27:11 -0300

    Arquivo Main
    Criado arquivo de principal de execucao

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# -*- coding: Latin-1 -*-
+
+#Autor: Israel Gomes
+#Data: 26/08/2018
+import sqlite3
+
+class Elements:
+	def __init__(self): pass
+
+
+class Forms:
+	def __init__(self): pass
+
+
+class Interface:
+	def __init__(self): pass
+
+
+
+
